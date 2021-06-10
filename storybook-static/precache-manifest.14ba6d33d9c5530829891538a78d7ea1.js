664d0a1153c6daec4b17efd621f91736125feae5
yanni <yanni.mamporiya@gmail.com>
2021-06-11 00:39:03 +0300

    beginig of documentation

---
+++ storybook-static/precache-manifest.14ba6d33d9c5530829891538a78d7ea1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "url": "main.c054bb738f9be5845ae7.bundle.js"
+  },
+  {
+    "url": "runtime~main.c054bb738f9be5845ae7.bundle.js"
+  },
+  {
+    "url": "vendors~main.c054bb738f9be5845ae7.bundle.js"
+  },
+  {
+    "url": "vendors~main.c054bb738f9be5845ae7.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a96237df5b87c3fde2c437d8b79d639",
+    "url": "iframe.html"
+  }
+];
